54e08b405f433cf9ba7f60b2c2b1fed837a4d474
Justino José <justinojosesilva@gmail.com>
2021-09-19 18:46:03 -0300

    Desafio 04 concluido

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    try {
+      const listUsers = this.listAllUsersUseCase.execute({
+        user_id: `${user_id}`,
+      });
+      return response.json(listUsers);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
